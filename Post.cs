8dae0c8d23a6ed0d357fa44433c5cd9a13355a67
Su Mon <sumonnwe@gmail.com>
2025-05-21 15:55:49 +0800

    Code First Migration

---
+++ Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationsDemo
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        [MaxLength(200)]
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public string Abstract { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MigrationsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BlogContext())
+            {
+                db.Blogs.Add(new Blog { Name = "Another Blog " });
+                db.SaveChanges();
+
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(blog.Name);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
